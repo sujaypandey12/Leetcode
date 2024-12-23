afbd57546ce42141c3d1bea741a6fd1653f16aa2
sujaypandey12 <133794237+sujaypandey12@users.noreply.github.com>
2024-12-23 18:39:47 +0530

    Time: 412 ms (33.69%), Space: 235.4 MB (8.41%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    int helper(vector<int> & prices,int idx,int buy,int cnt,vector<vector<vector<int>>> &dp){
-        //base case
-        if(cnt==0) return 0;
-        if(idx==prices.size()){
-          return 0;
-        }
-        int profit1=0;
-        int profit2=0;
-        if(dp[idx][buy][cnt]!=-1){
-            return dp[idx][buy][cnt];
-        }
-        if(buy==1){
-          profit1= max(-prices[idx]+helper(prices,idx+1,0,cnt,dp),helper(prices,idx+1,1,cnt,dp));
-        }else{
-    
-        profit2= max(prices[idx]+helper(prices,idx+1,1,cnt-1,dp),helper(prices,idx+1,0,cnt,dp));
-        }
-        
-        return dp[idx][buy][cnt]=max(profit1,profit2);
-
-    }
+   
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>> (2,vector<int>(3,-1)));
-        return helper(prices,0,1,2,dp);
+        vector<vector<vector<long long>>> dp(n+1,vector<vector<long long>> (2,vector<long long>(3,0)));
+        //try to cover the first test case
+       
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=1;j++){
+                for(int k=1;k<=2;k++){
+                    if(j==1){
+                        dp[i][j][k]=max(-prices[i]+dp[i+1][0][k],dp[i+1][1][k]);
+                    }else{
+                        dp[i][j][k]=max(prices[i]+dp[i+1][1][k-1],dp[i+1][0][k]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];
+
     }
 };
