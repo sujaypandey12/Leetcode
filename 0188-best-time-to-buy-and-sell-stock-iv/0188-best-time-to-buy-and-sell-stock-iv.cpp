78523a154ab4b7ec44022a24aee702fec9fd83ae
sujaypandey12 <133794237+sujaypandey12@users.noreply.github.com>
2024-12-24 10:53:43 +0530

    Time: 6 ms (64.34%), Space: 18.2 MB (27.94%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -17,8 +17,23 @@ public:
     }
     int maxProfit(int k, vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
-        return helper(prices,0,1,k,dp);
+      //  vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
+       // return helper(prices,0,1,k,dp);
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
+        // i am not going to make changes to base case as i alreasy inttilised woth 0
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=1;j++){
+                for(int t=1;t<=k;t++){
+                    if(j==1){
+                        dp[i][j][t]=max(-prices[i]+dp[i+1][0][t],dp[i+1][1][t]);
+                    }else{
+                        dp[i][j][t]=max(prices[i]+dp[i+1][1][t-1],dp[i+1][0][t]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][k];
+
         
     }
 };
